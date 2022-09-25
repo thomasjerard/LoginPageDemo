e3b11240480fb435e820b3576e9b0bbfd4fc9e0a
Thomas Jerard <74585794+thomasjerard@users.noreply.github.com>
2022-09-26 01:57:01 +0530

    Add files via upload
    files upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var username = document.getElementById("username").value;
+document.querySelector(".SignIn").addEventListener("click",function(){
+window.open("https://www.google.com/","_self");
+alert(username);
+});
